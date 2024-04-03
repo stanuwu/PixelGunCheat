627aea003ea4e52e8903083a55f8a75bbdddf19e
stan <77831846+stanuwu@users.noreply.github.com>
2024-04-03 02:50:25 +0200

    initial commit

---
+++ CheatManager.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BepInEx.Logging;
+using PixelGunCheat.util;
+using Steamworks;
+using UnityEngine;
+using Logger = BepInEx.Logging.Logger;
+using Renderer = PixelGunCheat.util.Renderer;
+
+namespace PixelGunCheat
+{
+    public class CheatManager : MonoBehaviour
+    {
+        private long tickCount = 0;
+        private GameController gameController;
+        private List<Player_move_c> playerList = new();
+        private Player_move_c player;
+        private bool _initMat = false;
+        private ManualLogSource logger = Logger.CreateLogSource("Cheat");
+        private Vector3 aimedPos = Vector3.zero;
+
+        private void Awake()
+        {
+            Debug.Log("Loaded Cheat");
+            gameObject.hideFlags = HideFlags.HideAndDontSave;
+        }
+
+        private void OnDestroy()
+        {
+            Debug.Log("Destroyed Cheat");
+        }
+
+        private void Update()
+        {
+            tickCount++;
+            if (tickCount % 5 == 0)
+            {
+                gameController = FindObjectOfType<GameController>();
+                playerList = FindObjectsOfType<Player_move_c>().ToList();
+                player = playerList.Find(p => p.nickLabel.text == "1111");
+            }
+            if (gameController == null) return;
+            if (player == null) return;
+            player.MaxBackpackAmmo();
+
+            Camera main = Camera.main;
+            if (main == null) return;
+
+            Vector3 aimPos = main.transform.position;
+            Player_move_c target = null;
+            double distance = double.MaxValue;
+            foreach (var p in playerList)
+            {
+                if(p.nickLabel.text == "1111") continue;
+                if(!(p.nickLabel.color.r == 1 && p.nickLabel.color.g == 0 && p.nickLabel.color.b == 0)) continue;
+                if(!PlayerUtil.IsVisible(PlayerUtil.WorldToScreenPoint(main, p.transform.position))) continue;
+
+                Vector3 headPos = p.transform.position + new Vector3(0, 0.75f, 0);
+                
+                if(Vector3.Distance(headPos, aimPos) > 600) continue;
+
+                Vector3 aimDirection = main.transform.rotation * new Vector3(1, 1, 1);
+                Vector3 v = headPos - aimPos;
+                float d = Vector3.Dot(v, aimDirection);
+                Vector3 closestPoint = aimPos + aimDirection * d;
+
+                float newDist = Vector3.Distance(closestPoint, headPos);
+                if (!(distance > newDist)) continue;
+
+                RaycastHit hit;
+                if (Physics.Raycast(new Ray(aimPos, Vector3.Normalize(headPos - aimPos)), out hit, 600))
+                {
+                    if (hit.colliderInstanceID != p.headCollider.GetInstanceID()) continue;
+                }
+                else
+                {
+                    continue;
+                }
+
+                distance = newDist;
+                target = p;
+            }
+
+            if (target != null)
+            {
+                main.transform.LookAt(target.transform.position + new Vector3(0, 0.75f, 0));
+                aimedPos = target.transform.position + new Vector3(0, 0.75f, 0);
+            }
+            else
+            {
+                aimedPos = Vector3.zero;
+            }
+        }
+
+        private void OnGUI()
+        {
+            if (!_initMat)
+            {
+                Renderer.InitMat();
+                _initMat = true;
+            }
+            Renderer.DrawString(new Vector2(26, 26), "PixelGunCheat :3", Color.black, false);
+            Renderer.DrawString(new Vector2(25, 25), "PixelGunCheat :3", Color.cyan, false);
+            Renderer.DrawString(new Vector2(26, 101), "github.com/stanuwu", Color.black, false);
+            Renderer.DrawString(new Vector2(25, 100), "github.com/stanuwu", Color.cyan, false);
+            if (gameController == null) return;
+            Camera main = Camera.main;
+            if (main == null) return;
+            foreach (var playerMoveC in playerList.OrderByDescending(p => Vector3.Distance(p.transform.position, main.transform.position)))
+            {
+                Vector3 position = playerMoveC.transform.position;
+                Vector3 topWorld = position + new Vector3(0, 2, 0);
+                
+                Vector3 screenPos = PlayerUtil.WorldToScreenPoint(main, position);
+                Vector3 topScreen = PlayerUtil.WorldToScreenPoint(main, topWorld);
+
+                if(screenPos.z < 0) continue;
+                
+                if(!PlayerUtil.IsVisible(screenPos)) continue;
+                float scaledDist = screenPos.y - topScreen.y;
+                
+                Renderer.DrawCenteredBox(new Vector2(screenPos.x + 1, screenPos.y + 1), new Vector2(scaledDist, scaledDist * 1.5f), Color.black, 2);
+                Renderer.DrawCenteredBox(screenPos, new Vector2(scaledDist, scaledDist * 1.5f), playerMoveC.nickLabel.color, 2);
+            }
+            if (aimedPos == Vector3.zero) return;
+            Vector3 ap = PlayerUtil.WorldToScreenPoint(main, aimedPos);
+            Renderer.DrawCenteredBox(new Vector2(ap.x + 1, ap.y + 1), new Vector2(50, 50), Color.black, 3);
+            Renderer.DrawCenteredBox(ap, new Vector2(50, 50), Color.magenta, 2);
+        }
+    }
+}
---
+++ util/Renderer.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+namespace PixelGunCheat.util
+{
+    public static class Renderer
+    {
+        public static GUIStyle StringStyle { get; set; } = new GUIStyle(GUI.skin.label);
+        public static Material Mat;
+        public static Color Color
+        {
+            get { return GUI.color; }
+            set { GUI.color = value; }
+        }
+
+        public static void DrawLine(Vector2 from, Vector2 to, float thickness, Color color)
+        {
+            Color = color;
+            DrawLine(from, to, thickness);
+        }
+
+        public static void DrawLine(Vector2 from, Vector2 to, float thickness)
+        {
+            var delta = (to - from).normalized;
+            var angle = Mathf.Atan2(delta.y, delta.x) * Mathf.Rad2Deg;
+            GUIUtility.RotateAroundPivot(angle, from);
+            DrawBox(from, Vector2.right * (from - to).magnitude, thickness);
+            GUIUtility.RotateAroundPivot(-angle, from);
+        }
+
+        public static void DrawSquare(Vector2 position, Vector2 size, Color color)
+        {
+            Color = color;
+            GUI.DrawTexture(new Rect(position.x, position.y, size.x, size.y), Texture2D.whiteTexture);
+        }
+
+        public static void DrawCenteredSquare(Vector2 position, Vector2 size, Color color)
+        {
+            Color = color;
+            GUI.DrawTexture(new Rect(position.x - (size.x / 2), position.y - (size.y / 2), size.x, size.y), Texture2D.whiteTexture);
+        }
+
+        public static void DrawCenteredBox(Vector2 position, Vector2 size, Color color, float thickness)
+        {
+            Color = color;
+            DrawBox(new Vector2(position.x - size.x / 2, position.y - size.y / 2), size, thickness);
+        }
+        
+        public static void DrawBox(Vector2 position, Vector2 size, float thickness)
+        {
+            GUI.DrawTexture(new Rect(position.x, position.y, size.x, thickness), Texture2D.whiteTexture);
+            GUI.DrawTexture(new Rect(position.x, position.y, thickness, size.y), Texture2D.whiteTexture);
+            GUI.DrawTexture(new Rect(position.x + size.x, position.y, thickness, size.y), Texture2D.whiteTexture);
+            GUI.DrawTexture(new Rect(position.x, position.y + size.y, size.x + thickness, thickness), Texture2D.whiteTexture);
+        }
+
+        public static void DrawString(Vector2 position, string label, Color color, bool centered = true)
+        {
+            Color = color;
+            DrawString(position, label, centered);
+        }
+
+        public static void DrawString(Vector2 position, string label, bool centered = true)
+        {
+            var content = new GUIContent(label);
+            var size = StringStyle.CalcSize(content);
+            var upperLeft = centered ? position - size / 2f : position;
+            GUI.Label(new Rect(upperLeft, size), content);
+        }
+
+        public static void InitMat()
+        {
+            Material mat = new Material(Shader.Find("Hidden/Internal-Colored"));
+            mat.hideFlags = HideFlags.HideAndDontSave;
+            mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            mat.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+            mat.SetInt("_ZWrite", 0);
+            Mat = mat;
+            GUI.skin.label.fontSize = 64;
+            StringStyle.fontSize = 64;
+        }
+    }
+}
